2bcfdc2052773833db92348765e4384a8284579c
stereobooster <stereobooster@gmail.com>
2018-01-25 18:28:03 +0100

    307 redirect example

--- README.md
+++ README.md
@@ -1,77 +1,9 @@
 # puppeteer-example
 
-gets critical css
+307 redirect example
 
 ```
 yarn test
-.external {
-  color: red;
-  font-family: "Roboto", sans-serif;
-  animation: swing 1s ease;
-  animation-iteration-count: 1;
-}
+<!DOCTYPE html><html lang="en"><head>
+...
 ```
-
-## Issues with Chrome CSS coverage
-
-### Doesn't show `@font-face`
-
-should show
-
-```css
-@font-face {
-  font-family: "Roboto";
-  ...
-}
-```
-
-because of
-
-```css
-.external {
-  font-family: "Roboto", sans-serif;
-}
-```
-
-### Doesn't show `@keyframes`
-
-should show
-
-```css
-@keyframes swing {
-  ...
-}
-```
-
-because of
-
-```css
-.external {
-  animation: swing 1s ease;
-}
-```
-
-### Doesn't show `:hover`
-
-should show
-
-```css
-.external:hover {
-  color: green;
-}
-```
-
-because of
-
-```css
-.external {
-  ...
-}
-```
-
-## Related
-
-- https://bugs.chromium.org/p/chromium/issues/detail?id=765088
-- https://bugs.chromium.org/p/chromium/issues/detail?id=744202&q=css%20Coverage%20&colspec=ID%20Pri%20M%20Stars%20ReleaseBlock%20Component%20Status%20Owner%20Summary%20OS%20Modified
-- https://bugs.chromium.org/p/chromium/issues/detail?id=734470&q=css%20Coverage%20&colspec=ID%20Pri%20M%20Stars%20ReleaseBlock%20Component%20Status%20Owner%20Summary%20OS%20Modified
-- https://bugs.chromium.org/p/chromium/issues/detail?id=717195&q=css%20Coverage%20&colspec=ID%20Pri%20M%20Stars%20ReleaseBlock%20Component%20Status%20Owner%20Summary%20OS%20Modified
--- index.js
+++ index.js
@@ -7,64 +7,26 @@ const puppeteer = require("puppeteer");
 const options = {
   port: 12345,
   sourceDir: path.normalize(`${process.cwd()}/public`)
-}
+};
 
 const startServer = options => {
-   const app = express()
-     .use("/", serveStatic(options.sourceDir))
-   const server = http.createServer(app);
-   server.listen(options.port);
-   return server;
- };
+  const app = express().use("/", serveStatic(options.sourceDir));
+  app.get("/307", function(request, response) {
+    response.redirect(307, "/");
+  });
+  const server = http.createServer(app);
+  server.listen(options.port);
+  return server;
+};
 
 (async () => {
-  const server = startServer(options)
-
+  const server = startServer(options);
   const browser = await puppeteer.launch();
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  //Start sending raw DevTools Protocol commands are sent using `client.send()`
-  //First off enable the necessary "Domains" for the DevTools commands we care about
-  const client = await page.target().createCDPSession()
-  await client.send('Page.enable')
-  await client.send('DOM.enable')
-  await client.send('CSS.enable')
-
-  const inlineStylesheetIndex = new Set();
-  client.on('CSS.styleSheetAdded', stylesheet => {
-    const { header } = stylesheet
-    if (header.isInline || header.sourceURL === '' || header.sourceURL.startsWith('blob:')) {
-      inlineStylesheetIndex.add(header.styleSheetId);
-    }
-  });
-
-  //Start tracking CSS coverage
-  await client.send('CSS.startRuleUsageTracking')
-
-  await page.goto(`http://localhost:${options.port}`)
-  // const content = await page.content();
-  // console.log(content);
-
-  const rules = await client.send('CSS.takeCoverageDelta')
-  const usedRules = rules.coverage.filter(rule => {
-    return rule.used
-  })
-
-  const slices = [];
-  for (const usedRule of usedRules) {
-    // console.log(usedRule.styleSheetId)
-    if (inlineStylesheetIndex.has(usedRule.styleSheetId)) {
-      continue;
-    }
-
-    const stylesheet = await client.send('CSS.getStyleSheetText', {
-      styleSheetId: usedRule.styleSheetId
-    });
-
-    slices.push(stylesheet.text.slice(usedRule.startOffset, usedRule.endOffset));
-  }
-
-  console.log(slices.join(''));
+  await page.goto(`http://localhost:${options.port}`);
+  const content = await page.content();
+  console.log(content);
 
   await page.close();
   await browser.close();
